c96d2d45d923b82dd247f31a08c85669ca36e714
GustavoHeimburg <gutogustavogu@gmail.com>
2024-05-20 14:44:20 -0300

    Treinos Java

--- src/main/java/ProjetosTreinos/Gerarid.java
+++ src/main/java/ProjetosTreinos/Gerarid.java
@@ -1,4 +1,4 @@
-package ProjetosTreinos.database;
+package ProjetosTreinos;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -18,9 +18,11 @@ public class Gerarid {
         String password = scanner.nextLine();
 
         registerUser(username, password);
-
-        System.out.println("Digite seu nome de usuário e senha para login:");
+    System.out.println("precione (ENTER) e faça login com o cadastro que criou anteriormente. 'O login é obrigatorio para receber o seu ID unico de usuario!'");
+        String lerProxLinha = scanner.nextLine();
+        System.out.println("Digite seu nome:");
         String loginUsername = scanner.nextLine();
+        System.out.println("Digte sua senha:");
         String loginPassword = scanner.nextLine();
 
         if (authenticateUser(loginUsername, loginPassword)) {
@@ -28,6 +30,25 @@ public class Gerarid {
         } else {
             System.out.println("Falha no login. Verifique seu nome de usuário e senha.");
         }
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Digite 'd':");
+//        String input1 = scanner.nextLine();
+//
+//        System.out.println("Digite 'e':");
+//        String input2 = scanner.nextLine();
+//
+//        System.out.println("Digite 'human':");
+//        String input3 = scanner.nextLine();
+//
+//        if (input1.equals("d") && input2.equals("e") && input3.equals("human")) {
+//            System.out.println("Autenticação bem-sucedida!");
+//        } else {
+//            System.out.println("Autenticação falhou!");
+//        }
+
+
     }
 
     public static void registerUser(String username, String password) {
